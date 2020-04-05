3a0f1ef54bce8de6752161646c36e52e50f3d2bc
Kouki Uchida <uchidakouki264@gmail.com>
2020-04-06 07:34:44 +0900

    ユーザーテーブルのシーダーのモデル名前空間を修正

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserTableSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class)->create();
+        factory(User::class)->create();
     }
 }
--- database/seeds/UserTestTableSeeder.php
+++ database/seeds/UserTestTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserTestTableSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class UserTestTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class)->states('test')->create();
+        factory(User::class)->states('test')->create();
     }
 }
