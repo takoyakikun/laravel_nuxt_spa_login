3dd2833e139a7d5101e9ff770671b22b25902886
Kouki Uchida <uchidakouki264@gmail.com>
2020-04-06 07:45:07 +0900

    ユーザー権限を追加

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'role' => 10, // 一般ユーザー
         'remember_token' => Str::random(10),
     ];
 });
@@ -30,5 +31,6 @@ $factory->state(User::class, 'test', function (Faker $faker) {
     return [
         'name' => 'テスト',
         'email' => 'test@test.com',
+        'role' => 1, // 開発者
     ];
 });
