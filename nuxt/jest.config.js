af0940e4ff3447d3fa96a284337a700b860cfd2c
Kouki Uchida <uchidakouki264@gmail.com>
2020-08-09 09:07:22 +0900

    test時のimportエラーを修正

--- nuxt/jest.config.js
+++ nuxt/jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
-    "^~/(.*)$": "<rootDir>/$1"
+    "^~/(.*)$": "<rootDir>/$1",
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/styleMock.js"
   },
   moduleFileExtensions: ["js", "vue", "json"],
   transform: {
     "^.+\\.js$": "babel-jest",
     ".*\\.(vue)$": "vue-jest"
   },
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/(?!vuetify/lib)(?!vee-validate/dist/rules)"
+  ],
   collectCoverage: true,
   collectCoverageFrom: [
     "<rootDir>/components/**/*.vue",
