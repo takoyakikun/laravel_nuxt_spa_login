e618b4089565322deee472ea03f1608bf98097ee
Kouki Uchida <uchidakouki264@gmail.com>
2020-08-31 18:25:16 +0900

    ユーザー追加テストを修正

--- nuxt/test/components/register/register.spec.js
+++ nuxt/test/components/register/register.spec.js
@@ -1,34 +1,43 @@
-import { shallowMount } from "@vue/test-utils"
+import { createLocalVue, mount } from "@vue/test-utils"
 import Vuetify from "vuetify"
 import Vuex from "vuex"
+import VueRouter from "vue-router"
 import axios from "axios"
 import storeConfig from "@/test/storeConfig"
-import { ValidationProvider, ValidationObserver } from "vee-validate"
-import { VTextField } from "vuetify/lib"
+import setConfigData from "@/test/setConfigData"
 import Register from "@/components/register/register"
-import UserForm from "@/components/users/userForm"
 
-let store = new Vuex.Store(storeConfig)
-let vuetify = new Vuetify()
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(VueRouter)
+
+const router = new VueRouter()
+const vuetify = new Vuetify()
 
 jest.useFakeTimers()
 
-describe("components/register", () => {
-  describe("register", () => {
+let store
+beforeEach(() => {
+  store = new Vuex.Store(storeConfig)
+  store.commit("config/setConfig", setConfigData)
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+describe("components/register/register", () => {
+  describe("mount", () => {
     let wrapper
     beforeEach(() => {
-      const router = { push: jest.fn() }
-      wrapper = shallowMount(Register, {
+      router.push = jest.fn()
+
+      wrapper = mount(Register, {
+        localVue,
         store,
+        router,
         vuetify,
-        stubs: {
-          ValidationObserver,
-          ValidationProvider,
-          UserForm,
-          VTextField
-        },
-        sync: false,
-        mocks: { $router: router }
+        sync: false
       })
     })
 
@@ -36,221 +45,216 @@ describe("components/register", () => {
       expect(wrapper.vm).toBeTruthy()
     })
 
-    test("ユーザー追加失敗(フロント側エラー)", async () => {
-      // spyOn
-      const registerFormValidation = jest.spyOn(
-        wrapper.vm.$refs.registerForm,
-        "validate"
-      )
-      const axiosPost = jest.spyOn(axios, "post")
-
-      // エラーレスポンス
-      const response = {
-        status: 422
-      }
-      // axiosのレスポンスをモックする
-      axios.post.mockImplementation(url => {
-        return Promise.resolve(response)
-      })
-      wrapper.vm.$store.$axios = axios
-
-      // ユーザー追加処理
-      await wrapper.vm.submit()
-      jest.runAllTimers()
-
-      // バリデーションチェックをした
-      expect(registerFormValidation).toHaveBeenCalled()
-
-      // バリデーションエラー
-      expect(
-        Object.keys(wrapper.vm.$refs.registerForm.errors)
-      ).not.toHaveLength(0)
-
-      // API送信をしない
-      expect(axiosPost).not.toHaveBeenCalled()
-    })
-
-    test("ユーザー追加失敗(API側エラー)", async () => {
-      // spyOn
-      const registerFormValidation = jest.spyOn(
-        wrapper.vm.$refs.registerForm,
-        "validate"
-      )
-      const axiosPost = jest.spyOn(axios, "post")
-
-      // エラーレスポンス
-      const response = {
-        status: 422
-      }
-      // axiosのレスポンスをモックする
-      axios.post.mockImplementation(url => {
-        return Promise.resolve(response)
-      })
-      wrapper.vm.$store.$axios = axios
-
-      // フォームを入力してユーザー追加処理
-      wrapper.find("input[name='name']").setValue("テスト")
-      wrapper.find("input[name='email']").setValue("test@test.com")
-      wrapper.find("input[name='password']").setValue("password")
-      wrapper.find("input[name='password_confirmation']").setValue("password")
-      await wrapper.vm.submit()
-      jest.runAllTimers()
-
-      // バリデーションチェックをした
-      expect(registerFormValidation).toHaveBeenCalled()
-
-      // API送信をした
-      expect(axiosPost).toHaveBeenCalled()
-      expect(axiosPost).toHaveBeenCalledWith("/api/register", {
-        name: "テスト",
-        email: "test@test.com",
-        password: "password",
-        password_confirmation: "password"
+    describe("ユーザー追加", () => {
+      let registerFormValidation
+      let axiosPost
+      beforeEach(() => {
+        // spyOn
+        registerFormValidation = jest.spyOn(
+          wrapper.vm.$refs.registerForm,
+          "validate"
+        )
+        axiosPost = jest.spyOn(axios, "post")
       })
 
-      // snackbarのエラー表示
-      expect(wrapper.vm.$store.getters["snackbar/text"]).toBe(
-        "新規ユーザーの作成に失敗しました。"
-      )
-      expect(wrapper.vm.$store.getters["snackbar/options"].color).toBe("error")
-    })
-
-    test("ユーザー追加成功したが認証で失敗", async () => {
-      // spyOn
-      const registerFormValidation = jest.spyOn(
-        wrapper.vm.$refs.registerForm,
-        "validate"
-      )
-      const axiosPost = jest.spyOn(axios, "post")
-      const routerPush = jest.spyOn(wrapper.vm.$router, "push")
-
-      // 正常なレスポンス
-      const response = {
-        status: 200
-      }
-      // エラーレスポンス
-      const responseError = {
-        status: 422
-      }
-      // axiosのレスポンスをモックする
-      axios.post
-        .mockImplementationOnce(url => {
-          return Promise.resolve(response)
+      describe("失敗", () => {
+        beforeEach(() => {
+          // エラーレスポンス
+          const response = {
+            status: 422
+          }
+          // axiosのレスポンスをモックする
+          axios.post.mockImplementation(url => {
+            return Promise.resolve(response)
+          })
+          wrapper.vm.$store.$axios = axios
         })
-        .mockImplementationOnce(url => {
-          return Promise.resolve(responseError)
+
+        test("フロント側エラー", async () => {
+          // ユーザー追加処理
+          await wrapper.vm.submit()
+          jest.runAllTimers()
+
+          // バリデーションチェックをした
+          expect(registerFormValidation).toHaveBeenCalled()
+
+          // バリデーションエラー
+          expect(
+            Object.keys(wrapper.vm.$refs.registerForm.errors)
+          ).not.toHaveLength(0)
+
+          // API送信をしない
+          expect(axiosPost).not.toHaveBeenCalled()
         })
-      wrapper.vm.$store.$axios = axios
 
-      // フォームを入力してログイン処理
-      wrapper.find("input[name='name']").setValue("テスト")
-      wrapper.find("input[name='email']").setValue("test@test.com")
-      wrapper.find("input[name='password']").setValue("password")
-      wrapper.find("input[name='password_confirmation']").setValue("password")
-      await wrapper.vm.submit()
-      jest.runAllTimers()
+        test("API側エラー", async () => {
+          // フォームを入力してユーザー追加処理
+          wrapper.find("input[name='name']").setValue("テスト")
+          wrapper.find("input[name='email']").setValue("test@test.com")
+          wrapper.find("input[name='password']").setValue("password")
+          wrapper
+            .find("input[name='password_confirmation']")
+            .setValue("password")
+          await wrapper.vm.submit()
+          jest.runAllTimers()
 
-      // バリデーションチェックをした
-      expect(registerFormValidation).toHaveBeenCalled()
+          // バリデーションチェックをした
+          expect(registerFormValidation).toHaveBeenCalled()
 
-      // API送信をした
-      expect(axiosPost).toHaveBeenCalled()
-      expect(axiosPost).toHaveBeenCalledWith("/api/register", {
-        name: "テスト",
-        email: "test@test.com",
-        password: "password",
-        password_confirmation: "password"
-      })
-      expect(axiosPost).toHaveBeenCalledWith("/api/login", {
-        email: "test@test.com",
-        password: "password"
-      })
+          // API送信をした
+          expect(axiosPost).toHaveBeenCalled()
+          expect(axiosPost).toHaveBeenCalledWith("/api/register", {
+            name: "テスト",
+            email: "test@test.com",
+            password: "password",
+            password_confirmation: "password"
+          })
 
-      // ログインページへリダイレクトした
-      expect(routerPush).toHaveBeenCalled()
-      expect(routerPush).toHaveBeenCalledWith("/login")
-
-      // snackbarのエラー表示
-      expect(wrapper.vm.$store.getters["snackbar/text"]).toBe(
-        "認証に失敗しました。"
-      )
-      expect(wrapper.vm.$store.getters["snackbar/options"].color).toBe("error")
-    })
-
-    test("ユーザー追加成功", async () => {
-      // spyOn
-      const registerFormValidation = jest.spyOn(
-        wrapper.vm.$refs.registerForm,
-        "validate"
-      )
-      const axiosPost = jest.spyOn(axios, "post")
-      const routerPush = jest.spyOn(wrapper.vm.$router, "push")
-
-      // 正常なレスポンス
-      const response = {
-        status: 200
-      }
-      // axiosのレスポンスをモックする
-      axios.post.mockImplementation(url => {
-        return Promise.resolve(response)
-      })
-      wrapper.vm.$store.$axios = axios
-
-      // フォームを入力してログイン処理
-      wrapper.find("input[name='name']").setValue("テスト")
-      wrapper.find("input[name='email']").setValue("test@test.com")
-      wrapper.find("input[name='password']").setValue("password")
-      wrapper.find("input[name='password_confirmation']").setValue("password")
-      await wrapper.vm.submit()
-      jest.runAllTimers()
-
-      // バリデーションチェックをした
-      expect(registerFormValidation).toHaveBeenCalled()
-
-      // API送信をした
-      expect(axiosPost).toHaveBeenCalled()
-      expect(axiosPost).toHaveBeenCalledWith("/api/register", {
-        name: "テスト",
-        email: "test@test.com",
-        password: "password",
-        password_confirmation: "password"
-      })
-      expect(axiosPost).toHaveBeenCalledWith("/api/login", {
-        email: "test@test.com",
-        password: "password"
+          // snackbarのエラー表示
+          expect(wrapper.vm.$store.getters["snackbar/text"]).toBe(
+            "新規ユーザーの作成に失敗しました。"
+          )
+          expect(wrapper.vm.$store.getters["snackbar/options"].color).toBe(
+            "error"
+          )
+        })
       })
 
-      // メール認証ページへリダイレクトした
-      expect(routerPush).toHaveBeenCalled()
-      expect(routerPush).toHaveBeenCalledWith("/resend")
+      describe("成功", () => {
+        let routerPush
+        beforeEach(() => {
+          // spyOn
+          routerPush = jest.spyOn(wrapper.vm.$router, "push")
+        })
 
-      // snackbarの完了表示
-      expect(wrapper.vm.$store.getters["snackbar/text"]).toBe(
-        "新規ユーザーを作成しました。"
-      )
-      expect(wrapper.vm.$store.getters["snackbar/options"].color).toBe(
-        "success"
-      )
-    })
+        test("認証失敗", async () => {
+          // 正常なレスポンス
+          const response = {
+            status: 200
+          }
+          // エラーレスポンス
+          const responseError = {
+            status: 422
+          }
+          // axiosのレスポンスをモックする
+          axios.post
+            .mockImplementationOnce(url => {
+              return Promise.resolve(response)
+            })
+            .mockImplementationOnce(url => {
+              return Promise.resolve(responseError)
+            })
+          wrapper.vm.$store.$axios = axios
 
-    test("loading中はログイン処理不可", async () => {
-      // spyOn
-      const registerFormValidation = jest.spyOn(
-        wrapper.vm.$refs.registerForm,
-        "validate"
-      )
+          // フォームを入力してログイン処理
+          wrapper.find("input[name='name']").setValue("テスト")
+          wrapper.find("input[name='email']").setValue("test@test.com")
+          wrapper.find("input[name='password']").setValue("password")
+          wrapper
+            .find("input[name='password_confirmation']")
+            .setValue("password")
+          await wrapper.vm.submit()
+          jest.runAllTimers()
 
-      // loading中の設定
-      wrapper.setData({
-        loading: true
+          // バリデーションチェックをした
+          expect(registerFormValidation).toHaveBeenCalled()
+
+          // API送信をした
+          expect(axiosPost).toHaveBeenCalled()
+          expect(axiosPost).toHaveBeenCalledWith("/api/register", {
+            name: "テスト",
+            email: "test@test.com",
+            password: "password",
+            password_confirmation: "password"
+          })
+          expect(axiosPost).toHaveBeenCalledWith("/api/login", {
+            email: "test@test.com",
+            password: "password"
+          })
+
+          // ログインページへリダイレクトした
+          expect(routerPush).toHaveBeenCalled()
+          expect(routerPush).toHaveBeenCalledWith("/login")
+
+          // snackbarのエラー表示
+          expect(wrapper.vm.$store.getters["snackbar/text"]).toBe(
+            "認証に失敗しました。"
+          )
+          expect(wrapper.vm.$store.getters["snackbar/options"].color).toBe(
+            "error"
+          )
+        })
+
+        test("認証成功", async () => {
+          // 正常なレスポンス
+          const response = {
+            status: 200
+          }
+          // axiosのレスポンスをモックする
+          axios.post.mockImplementation(url => {
+            return Promise.resolve(response)
+          })
+          wrapper.vm.$store.$axios = axios
+
+          // フォームを入力してログイン処理
+          wrapper.find("input[name='name']").setValue("テスト")
+          wrapper.find("input[name='email']").setValue("test@test.com")
+          wrapper.find("input[name='password']").setValue("password")
+          wrapper
+            .find("input[name='password_confirmation']")
+            .setValue("password")
+          await wrapper.vm.submit()
+          jest.runAllTimers()
+
+          // バリデーションチェックをした
+          expect(registerFormValidation).toHaveBeenCalled()
+
+          // API送信をした
+          expect(axiosPost).toHaveBeenCalled()
+          expect(axiosPost).toHaveBeenCalledWith("/api/register", {
+            name: "テスト",
+            email: "test@test.com",
+            password: "password",
+            password_confirmation: "password"
+          })
+          expect(axiosPost).toHaveBeenCalledWith("/api/login", {
+            email: "test@test.com",
+            password: "password"
+          })
+
+          // メール認証ページへリダイレクトした
+          expect(routerPush).toHaveBeenCalled()
+          expect(routerPush).toHaveBeenCalledWith("/resend")
+
+          // snackbarの完了表示
+          expect(wrapper.vm.$store.getters["snackbar/text"]).toBe(
+            "新規ユーザーを作成しました。"
+          )
+          expect(wrapper.vm.$store.getters["snackbar/options"].color).toBe(
+            "success"
+          )
+        })
       })
 
-      // ログイン処理
-      await wrapper.vm.submit()
-      jest.runAllTimers()
+      test("loading中はログイン処理不可", async () => {
+        // spyOn
+        const registerFormValidation = jest.spyOn(
+          wrapper.vm.$refs.registerForm,
+          "validate"
+        )
 
-      // バリデーションチェックをしない
-      expect(registerFormValidation).not.toHaveBeenCalled()
+        // loading中の設定
+        wrapper.setData({
+          loading: true
+        })
+
+        // ログイン処理
+        await wrapper.vm.submit()
+        jest.runAllTimers()
+
+        // バリデーションチェックをしない
+        expect(registerFormValidation).not.toHaveBeenCalled()
+      })
     })
   })
 })
