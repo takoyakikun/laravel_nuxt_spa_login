4eee5b2f0ea7f148181b59cc552eed9441fc2ad7
Kouki Uchida <uchidakouki264@gmail.com>
2020-12-06 23:52:01 +0900

    ミドルウェアのテスト処理を修正

--- nuxt/test/middleware/admin.spec.js
+++ nuxt/test/middleware/admin.spec.js
@@ -24,226 +24,217 @@ describe("auth", () => {
     expect(redirect).toHaveBeenCalledWith("/login")
   })
 
-  test("管理者権限以外でログインしているがメール認証をしていない", async () => {
-    // spyOn
-    const axiosGet = jest.spyOn(axios, "get")
+  describe("ログインしている", () => {
+    let axiosGet
+    beforeEach(() => {
+      // spyOn
+      axiosGet = jest.spyOn(axios, "get")
+    })
+    describe("管理者権限以外", () => {
+      let responseAdmin
+      beforeEach("", () => {
+        // spyOn
+        axiosGet = jest.spyOn(axios, "get")
+  
+        // 管理者アクセス権限レスポンス
+        responseAdmin = {
+          status: 200,
+          data: [false]
+        }
 
-    // 管理者アクセス権限レスポンス
-    const responseAdmin = {
-      status: 200,
-      data: [false]
-    }
-    // メール認証アクセス権限レスポンス
-    const responseVerified = {
-      status: 200,
-      data: [false]
-    }
-    // axiosのレスポンスをモックする
-    axios.get
-      .mockImplementationOnce(url => {
-        return Promise.resolve(responseAdmin)
+        // axiosのレスポンスをモックする
+        axios.get
+          .mockImplementationOnce(url => {
+            return Promise.resolve(responseAdmin)
+          })
+
+        // ログインユーザーデータをストアに追加
+        store.state.auth.user = {
+          name: "テスト",
+          email: "test@test.com",
+          role: 3
+        }
+
       })
-      .mockImplementationOnce(url => {
-        return Promise.resolve(responseVerified)
+
+      test("メール認証をしていない", async () => {
+        // メール認証アクセス権限レスポンス
+        const responseVerified = {
+          status: 200,
+          data: [false]
+        }
+        // axiosのレスポンスをモックする
+        axios.get
+          .mockImplementationOnce(url => {
+            return Promise.resolve(responseVerified)
+          })
+        store.$axios = axios
+
+        // ミドルウェアを実行
+        await Admin({ store: store, redirect: redirect })
+
+        // ログインしているのでtrue
+        expect(store.getters["auth/userExists"]).toBeTruthy()
+
+        // 管理者アクセス権限のAPI送信をした
+        expect(axiosGet).toHaveBeenCalled()
+        expect(axiosGet).toHaveBeenCalledWith("/api/permission/admin-higher")
+
+        // 管理者権限以外なのでfalse
+        expect(store.getters["auth/permission"]("admin-higher")).toBeFalsy()
+
+        // メール認証アクセス権限のAPI送信をした
+        expect(axiosGet).toHaveBeenCalled()
+        expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
+
+        // メール認証していないのでfalse
+        expect(store.getters["auth/permission"]("verified")).toBeFalsy()
+
+        // ログインページへリダイレクト
+        expect(redirect).toHaveBeenCalled()
+        expect(redirect).toHaveBeenCalledWith("/")
       })
-    store.$axios = axios
 
-    // ログインユーザーデータをストアに追加
-    store.state.auth.user = {
-      name: "テスト",
-      email: "test@test.com",
-      role: 10
-    }
+      test("メール認証している", async () => {
+        // メール認証アクセス権限レスポンス
+        const responseVerified = {
+          status: 200,
+          data: [true]
+        }
+        // axiosのレスポンスをモックする
+        axios.get
+          .mockImplementationOnce(url => {
+            return Promise.resolve(responseVerified)
+          })
+        store.$axios = axios
 
-    // ミドルウェアを実行
-    await Admin({ store: store, redirect: redirect })
+        // ミドルウェアを実行
+        await Admin({ store: store, redirect: redirect })
 
-    // ログインしているのでtrue
-    expect(store.getters["auth/userExists"]).toBeTruthy()
+        // ログインしているのでtrue
+        expect(store.getters["auth/userExists"]).toBeTruthy()
 
-    // 管理者アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/admin-higher")
+        // 管理者アクセス権限のAPI送信をした
+        expect(axiosGet).toHaveBeenCalled()
+        expect(axiosGet).toHaveBeenCalledWith("/api/permission/admin-higher")
 
-    // 管理者権限以外なのでfalse
-    expect(store.getters["auth/permission"]("admin-higher")).toBeFalsy()
+        // 管理者権限以外なのでfalse
+        expect(store.getters["auth/permission"]("admin-higher")).toBeFalsy()
 
-    // メール認証アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
+        // メール認証アクセス権限のAPI送信をした
+        expect(axiosGet).toHaveBeenCalled()
+        expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
 
-    // メール認証していないのでfalse
-    expect(store.getters["auth/permission"]("verified")).toBeFalsy()
+        // メール認証しているのでtrue
+        expect(store.getters["auth/permission"]("verified")).toBeTruthy()
 
-    // ログインページへリダイレクト
-    expect(redirect).toHaveBeenCalled()
-    expect(redirect).toHaveBeenCalledWith("/")
-  })
-
-  test("管理者権限以外でログインしていてメール認証もしている", async () => {
-    // spyOn
-    const axiosGet = jest.spyOn(axios, "get")
-
-    // 管理者アクセス権限レスポンス
-    const responseAdmin = {
-      status: 200,
-      data: [false]
-    }
-    // メール認証アクセス権限レスポンス
-    const responseVerified = {
-      status: 200,
-      data: [true]
-    }
-    // axiosのレスポンスをモックする
-    axios.get
-      .mockImplementationOnce(url => {
-        return Promise.resolve(responseAdmin)
+        // ログインページへリダイレクト
+        expect(redirect).toHaveBeenCalled()
+        expect(redirect).toHaveBeenCalledWith("/")
       })
-      .mockImplementationOnce(url => {
-        return Promise.resolve(responseVerified)
+    })
+
+    describe("管理者権限", () => {
+      let responseAdmin
+      beforeEach(() => {
+        // spyOn
+        axiosGet = jest.spyOn(axios, "get")
+  
+        // 管理者アクセス権限レスポンス
+        responseAdmin = {
+          status: 200,
+          data: [true]
+        }
+
+        // axiosのレスポンスをモックする
+        axios.get
+          .mockImplementationOnce(url => {
+            return Promise.resolve(responseAdmin)
+          })
+
+        // ログインユーザーデータをストアに追加
+        store.state.auth.user = {
+          name: "テスト",
+          email: "test@test.com",
+          role: 2
+        }
+
       })
-    store.$axios = axios
 
-    // ログインユーザーデータをストアに追加
-    store.state.auth.user = {
-      name: "テスト",
-      email: "test@test.com",
-      role: 5
-    }
+      test("メール認証をしていない", async () => {
+        // メール認証アクセス権限レスポンス
+        const responseVerified = {
+          status: 200,
+          data: [false]
+        }
+        // axiosのレスポンスをモックする
+        axios.get
+          .mockImplementationOnce(url => {
+            return Promise.resolve(responseVerified)
+          })
+        store.$axios = axios
 
-    // ミドルウェアを実行
-    await Admin({ store: store, redirect: redirect })
+        // ミドルウェアを実行
+        await Admin({ store: store, redirect: redirect })
 
-    // ログインしているのでtrue
-    expect(store.getters["auth/userExists"]).toBeTruthy()
+        // ログインしているのでtrue
+        expect(store.getters["auth/userExists"]).toBeTruthy()
 
-    // 管理者アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/admin-higher")
+        // 管理者アクセス権限のAPI送信をした
+        expect(axiosGet).toHaveBeenCalled()
+        expect(axiosGet).toHaveBeenCalledWith("/api/permission/admin-higher")
 
-    // 管理者権限以外なのでfalse
-    expect(store.getters["auth/permission"]("admin-higher")).toBeFalsy()
+        // 管理者権限なのでtrue
+        expect(store.getters["auth/permission"]("admin-higher")).toBeTruthy()
 
-    // メール認証アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
+        // メール認証アクセス権限のAPI送信をした
+        expect(axiosGet).toHaveBeenCalled()
+        expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
 
-    // メール認証しているのでtrue
-    expect(store.getters["auth/permission"]("verified")).toBeTruthy()
+        // メール認証していないのでfalse
+        expect(store.getters["auth/permission"]("verified")).toBeFalsy()
 
-    // ログインページへリダイレクト
-    expect(redirect).toHaveBeenCalled()
-    expect(redirect).toHaveBeenCalledWith("/")
-  })
-
-  test("管理者権限でログインしているがメール認証をしていない", async () => {
-    // spyOn
-    const axiosGet = jest.spyOn(axios, "get")
-
-    // 管理者アクセス権限レスポンス
-    const responseAdmin = {
-      status: 200,
-      data: [true]
-    }
-    // メール認証アクセス権限レスポンス
-    const responseVerified = {
-      status: 200,
-      data: [false]
-    }
-    // axiosのレスポンスをモックする
-    axios.get
-      .mockImplementationOnce(url => {
-        return Promise.resolve(responseAdmin)
+        // 認証メール再送信ページへリダイレクト
+        expect(redirect).toHaveBeenCalled()
+        expect(redirect).toHaveBeenCalledWith("/resend")
       })
-      .mockImplementationOnce(url => {
-        return Promise.resolve(responseVerified)
+
+      test("メール認証している", async () => {
+        // メール認証アクセス権限レスポンス
+        const responseVerified = {
+          status: 200,
+          data: [true]
+        }
+        // axiosのレスポンスをモックする
+        axios.get
+          .mockImplementationOnce(url => {
+            return Promise.resolve(responseVerified)
+          })
+        store.$axios = axios
+
+        // ミドルウェアを実行
+        await Admin({ store: store, redirect: redirect })
+
+        // ログインしているのでtrue
+        expect(store.getters["auth/userExists"]).toBeTruthy()
+
+        // 管理者アクセス権限のAPI送信をした
+        expect(axiosGet).toHaveBeenCalled()
+        expect(axiosGet).toHaveBeenCalledWith("/api/permission/admin-higher")
+
+        // 管理者権限なのでtrue
+        expect(store.getters["auth/permission"]("admin-higher")).toBeTruthy()
+
+        // メール認証アクセス権限のAPI送信をした
+        expect(axiosGet).toHaveBeenCalled()
+        expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
+
+        // メール認証しているのでtrue
+        expect(store.getters["auth/permission"]("verified")).toBeTruthy()
+
+        // リダイレクトしない
+        expect(redirect).not.toHaveBeenCalled()
       })
-    store.$axios = axios
-
-    // ログインユーザーデータをストアに追加
-    store.state.auth.user = {
-      name: "テスト",
-      email: "test@test.com",
-      role: 5
-    }
-
-    // ミドルウェアを実行
-    await Admin({ store: store, redirect: redirect })
-
-    // ログインしているのでtrue
-    expect(store.getters["auth/userExists"]).toBeTruthy()
-
-    // 管理者アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/admin-higher")
-
-    // 管理者権限なのでtrue
-    expect(store.getters["auth/permission"]("admin-higher")).toBeTruthy()
-
-    // メール認証アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
-
-    // メール認証していないのでfalse
-    expect(store.getters["auth/permission"]("verified")).toBeFalsy()
-
-    // 認証メール再送信ページへリダイレクト
-    expect(redirect).toHaveBeenCalled()
-    expect(redirect).toHaveBeenCalledWith("/resend")
-  })
-
-  test("管理者権限でログインしていてメール認証もしている", async () => {
-    // spyOn
-    const axiosGet = jest.spyOn(axios, "get")
-
-    // 管理者アクセス権限レスポンス
-    const responseAdmin = {
-      status: 200,
-      data: [true]
-    }
-    // メール認証アクセス権限レスポンス
-    const responseVerified = {
-      status: 200,
-      data: [true]
-    }
-    // axiosのレスポンスをモックする
-    axios.get
-      .mockImplementationOnce(url => {
-        return Promise.resolve(responseAdmin)
-      })
-      .mockImplementationOnce(url => {
-        return Promise.resolve(responseVerified)
-      })
-    store.$axios = axios
-
-    // ログインユーザーデータをストアに追加
-    store.state.auth.user = {
-      name: "テスト",
-      email: "test@test.com",
-      role: 5
-    }
-
-    // ミドルウェアを実行
-    await Admin({ store: store, redirect: redirect })
-
-    // ログインしているのでtrue
-    expect(store.getters["auth/userExists"]).toBeTruthy()
-
-    // 管理者アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/admin-higher")
-
-    // 管理者権限なのでtrue
-    expect(store.getters["auth/permission"]("admin-higher")).toBeTruthy()
-
-    // メール認証アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
-
-    // メール認証しているのでtrue
-    expect(store.getters["auth/permission"]("verified")).toBeTruthy()
-
-    // リダイレクトしない
-    expect(redirect).not.toHaveBeenCalled()
+    })
   })
 })
