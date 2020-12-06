e652f26df27abff0ba0c95619faad0a105a965a3
Kouki Uchida <uchidakouki264@gmail.com>
2020-12-07 02:50:43 +0900

    ミドルウェアのテスト処理を修正

--- nuxt/test/middleware/verified.spec.js
+++ nuxt/test/middleware/verified.spec.js
@@ -23,82 +23,77 @@ describe("auth", () => {
     expect(redirect).not.toHaveBeenCalled()
   })
 
-  test("ログインしているがメール認証をしていない", async () => {
-    // spyOn
-    const axiosGet = jest.spyOn(axios, "get")
+  describe("ログインしている", () => {
+    let axiosGet
+    beforeEach(() => {
+      // spyOn
+      axiosGet = jest.spyOn(axios, "get")
 
-    // メール認証アクセス権限レスポンス
-    const response = {
-      status: 200,
-      data: [false]
-    }
-    // axiosのレスポンスをモックする
-    axios.get.mockImplementation(url => {
-      return Promise.resolve(response)
+      // ログインユーザーデータをストアに追加
+      store.state.auth.user = {
+        name: "テスト",
+        email: "test@test.com",
+        role: 3
+      }
     })
-    store.$axios = axios
 
-    // ログインユーザーデータをストアに追加
-    store.state.auth.user = {
-      name: "テスト",
-      email: "test@test.com",
-      role: 10
-    }
+    test("メール認証をしていない", async () => {
+      // メール認証アクセス権限レスポンス
+      const response = {
+        status: 200,
+        data: [false]
+      }
+      // axiosのレスポンスをモックする
+      axios.get.mockImplementation(url => {
+        return Promise.resolve(response)
+      })
+      store.$axios = axios
 
-    // ミドルウェアを実行
-    await Verified({ store: store, redirect: redirect })
+      // ミドルウェアを実行
+      await Verified({ store: store, redirect: redirect })
 
-    // ログインしているのでtrue
-    expect(store.getters["auth/userExists"]).toBeTruthy()
+      // ログインしているのでtrue
+      expect(store.getters["auth/userExists"]).toBeTruthy()
 
-    // メール認証アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
+      // メール認証アクセス権限のAPI送信をした
+      expect(axiosGet).toHaveBeenCalled()
+      expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
 
-    // メール認証していないのでfalse
-    expect(store.getters["auth/permission"]("verified")).toBeFalsy()
+      // メール認証していないのでfalse
+      expect(store.getters["auth/permission"]("verified")).toBeFalsy()
 
-    // 認証メール再送信ページへリダイレクト
-    expect(redirect).toHaveBeenCalled()
-    expect(redirect).toHaveBeenCalledWith("/resend")
-  })
-
-  test("ログインしていてメール認証もしている", async () => {
-    // spyOn
-    const axiosGet = jest.spyOn(axios, "get")
-
-    // メール認証アクセス権限レスポンス
-    const response = {
-      status: 200,
-      data: [true]
-    }
-    // axiosのレスポンスをモックする
-    axios.get.mockImplementation(url => {
-      return Promise.resolve(response)
+      // 認証メール再送信ページへリダイレクト
+      expect(redirect).toHaveBeenCalled()
+      expect(redirect).toHaveBeenCalledWith("/resend")
     })
-    store.$axios = axios
 
-    // ログインユーザーデータをストアに追加
-    store.state.auth.user = {
-      name: "テスト",
-      email: "test@test.com",
-      role: 10
-    }
+    test("メール認証している", async () => {
+      // メール認証アクセス権限レスポンス
+      const response = {
+        status: 200,
+        data: [true]
+      }
+      // axiosのレスポンスをモックする
+      axios.get.mockImplementation(url => {
+        return Promise.resolve(response)
+      })
+      store.$axios = axios
 
-    // ミドルウェアを実行
-    await Verified({ store: store, redirect: redirect })
+      // ミドルウェアを実行
+      await Verified({ store: store, redirect: redirect })
 
-    // ログインしているのでtrue
-    expect(store.getters["auth/userExists"]).toBeTruthy()
+      // ログインしているのでtrue
+      expect(store.getters["auth/userExists"]).toBeTruthy()
 
-    // メール認証アクセス権限のAPI送信をした
-    expect(axiosGet).toHaveBeenCalled()
-    expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
+      // メール認証アクセス権限のAPI送信をした
+      expect(axiosGet).toHaveBeenCalled()
+      expect(axiosGet).toHaveBeenCalledWith("/api/permission/verified")
 
-    // メール認証しているのでtrue
-    expect(store.getters["auth/permission"]("verified")).toBeTruthy()
+      // メール認証しているのでtrue
+      expect(store.getters["auth/permission"]("verified")).toBeTruthy()
 
-    // リダイレクトしない
-    expect(redirect).not.toHaveBeenCalled()
+      // リダイレクトしない
+      expect(redirect).not.toHaveBeenCalled()
+    })
   })
 })
