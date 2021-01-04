7410d14c8aeede6da1c3c66d8a205a6ae8432a59
Kouki Uchida <uchidakouki264@gmail.com>
2021-01-04 13:51:56 +0900

    ログインしている場合のみ認証済ページの認証処理を修正

--- nuxt/middleware/verified.js
+++ nuxt/middleware/verified.js
@@ -1,12 +1,12 @@
 export default async function({ store, redirect, app }) {
-  // アクセス権限をAPIから取得してストアにセット
-  await app.$api.auth.checkAuth("verified")
+  // ログインしている場合のみ認証チェックをする
+  if (store.getters["auth/userExists"]) {
+    // アクセス権限をAPIから取得してストアにセット
+    await app.$api.auth.checkAuth("verified")
 
-  // ログイン中でメール認証済でない場合は認証メール再送信ページへリダイレクト
-  if (
-    store.getters["auth/userExists"] &&
-    !store.getters["auth/permission"]("verified")
-  ) {
-    return redirect("/resend")
+    // ログイン中でメール認証済でない場合は認証メール再送信ページへリダイレクト
+    if (!store.getters["auth/permission"]("verified")) {
+      return redirect("/resend")
+    }
   }
 }
